f6bb6d24fd24bc390209484c7107bb9b777c5389
amanda rae <amanda@amandarae.ca>
2013-04-14 11:36:22 -0400

    partials, update form to include form id

---
+++ partials/_course_form.php
@@ -0,0 +1,19 @@
+<form id="courseForm" action="query_service.php" method="post"  class="form-horizontal">
+<div class="control-group">
+ <label class='control-label' for='crn'>Course CRN</label> 
+  <div class='controls'> 
+    <input type='text' id='crn' name='crn' value=''>
+  </div>
+</div>
+<div class="control-group">
+ <label class='control-label' for='desc'>Course Description</label> 
+  <div class='controls'> 
+    <input type='text' id='desc' name='desc' value=''>
+  </div>
+</div>
+ <div class="control-group">
+ <label class='control-label' for='code'>Course Code</label> 
+  <div class='controls'> 
+    <input type='text' id='code' name='code' value=''>
+  </div>
+</div>
---
+++ partials/_teacher_form.php
@@ -0,0 +1,25 @@
+<form id="teacherForm" method="post" action="query_service.php" class="form-horizontal">
+<div class="control-group">
+ <label class='control-label' for='fName'>First Name</label> 
+  <div class='controls'> 
+    <input type='text' id='fName' name='fName' value=''>
+  </div>
+</div>
+ <div class="control-group">
+ <label class='control-label' for='lName'>Last Name</label> 
+  <div class='controls'> 
+    <input type='text' id='lName' name='lName' value=''>
+  </div>
+</div>
+ <div class="control-group">
+ <label class='control-label' for='uName'>User Name</label> 
+  <div class='controls'> 
+    <input type='text' id='uName' name='uName' value=''>
+  </div>
+</div>
+  <div class="control-group">
+ <label class='control-label' for='pass'>Password</label> 
+  <div class='controls'> 
+    <input type='text' id='pass' name='pass' value=''>
+  </div>
+</div>
