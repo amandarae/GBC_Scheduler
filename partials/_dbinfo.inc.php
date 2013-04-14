389925dfa2e20ba35561c6d718ab8754fd256f2e
amanda rae <amanda@amandarae.ca>
2013-04-14 19:01:10 -0400

    update for azure hosting

--- partials/_dbinfo.inc.php
+++ partials/_dbinfo.inc.php
@@ -3,10 +3,14 @@
     // $dbpassword = 'kHc16289!';
     // $dbhost = 'phpproj.db.10521321.hostedresource.com';
     // $dbdatabase = 'phpproj';
-    $dbusername = 'root';
-    $dbpassword = '';
-    $dbhost = 'localhost';
-    $dbdatabase = 'phpclassdb';
+    // $dbusername = 'root';
+    // $dbpassword = '';
+    // $dbhost = 'localhost';
+    // $dbdatabase = 'phpclassdb';
+    $dbusername = 'bccbdbc2ae2daf';
+    $dbpassword = 'afda806c';
+    $dbhost = 'us-cdbr-azure-east-b.cloudapp.net';
+    $dbdatabase = 'gbcminiAdIryX5DE';
     $bd = mysql_connect($dbhost, $dbusername, $dbpassword) 
 or die("Opps some thing went wrong");
 mysql_select_db($dbdatabase, $bd) or die("Oops some thing went wrong");
