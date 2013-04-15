f528bf34e41231b1a1ab883c5d2f013c5e3bbb6a
amanda rae <amanda@amandarae.ca>
2013-04-15 09:03:39 -0400

    tabbing fixes

--- partials/_dbinfo.inc.php
+++ partials/_dbinfo.inc.php
@@ -12,8 +12,8 @@
     $dbhost = 'us-cdbr-azure-east-b.cloudapp.net';
     $dbdatabase = 'gbcminiAdIryX5DE';
     $bd = mysql_connect($dbhost, $dbusername, $dbpassword) 
-or die("Opps some thing went wrong");
-mysql_select_db($dbdatabase, $bd) or die("Oops some thing went wrong");
+        or die("Oops some thing went wrong");
+    mysql_select_db($dbdatabase, $bd) or die("Oops some thing went wrong");
 
 function select($table, $params){
     if($params==='')
